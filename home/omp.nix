89317184f5948833a890c0989257b235553e5d00
Pedro Baracho <pedropbaracho@gmail.com>
2026-09-19 10:02:15 -0700

    Update omp to 18.2.6

--- home/omp.nix
+++ home/omp.nix
@@ -1,22 +1,22 @@
 { lib, stdenvNoCC, fetchurl }:
 let
-  version = "18.1.18";
+  version = "18.2.6";
   sources = {
     aarch64-darwin = {
       asset = "omp-darwin-arm64";
-      hash = "sha256-A1o13LJJ7bk5+gK3T8fA35sutlkHk0n7/7GI4bVYlXo=";
+      hash = "sha256-1JjaQNV34f+mgcqGMsLqQKn3IqCLiAQSAR033/7pUTo=";
     };
     x86_64-darwin = {
       asset = "omp-darwin-x64";
-      hash = "sha256-Kf6tG2Z9yWm4JcWqR5iq21t+ybLxXDMEsQGW16+kFx0=";
+      hash = "sha256-1ViAD6Mmq8aK48u7PlNjIuxkO9z93HHmqYiFfb4alKM=";
     };
     aarch64-linux = {
       asset = "omp-linux-arm64";
-      hash = "sha256-GugnPCMc64jOvJlxkBz39dl+1BSc3HQKgU9inNK03LI=";
+      hash = "sha256-ByRcvgUMOZmrXOqbq/6E5+iBnS9NXkm+9HwKrLa5V+Q=";
     };
     x86_64-linux = {
       asset = "omp-linux-x64";
-      hash = "sha256-RUIfml8RK8R8ufd8S015J2Mfj/hZYk+CEofshU6yOfw=";
+      hash = "sha256-DzhZjJHoI9jM4H8VHsOZnVHyE/ssw+B9ifGvju+SR6I=";
     };
   };
   source = sources.${stdenvNoCC.hostPlatform.system}
